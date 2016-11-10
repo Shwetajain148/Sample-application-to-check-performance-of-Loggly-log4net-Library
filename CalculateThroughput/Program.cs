6fc3afe6d17d91e1c81063a451b3696854eff708
shwetajain148 <shweta.jain@psquickit.com>
2016-11-10 12:13:22 +0530

    update sample app to calculate throughput in bulk mode

---
+++ CalculateThroughput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.loggly;
+using log4net;
+using System.Threading;
+
+
+namespace CalculateThroughput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger(typeof(Program));
+            Timer t = new Timer(printLogStatus, null, 0, 3 * 1000);
+            for (int i = 0; i < 20000; i++)
+            {
+                logger.Info(i + ":   Hi to all Serialization using Json.NET is even easier. In this next sample, a Dictionary of strings (similar to the one used above for deserialization) is declared and then serialized to JSON format.Hi to all Serialization using Json.NET is even easier. In this next sample, a Dictionary of strings (similar to the one used above for deserialization) is declared and then serialized to JSON format.");
+                //  Thread.Sleep(3*1000);
+            }
+
+            Console.ReadKey();
+
+        }
+        static void printLogStatus(Object o)
+        {
+            Console.WriteLine(LogglyClient.getLogStatus());
+            GC.Collect();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample-application-to-check-performance-of-Loggly-log4net-Library-
